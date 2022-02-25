b7cbb01fdb2ba87bfb670d5b94280919d1e6ade0
ErinMitt <33756931+ErinMitt@users.noreply.github.com>
2022-02-24 20:32:14 -0500

    added score yay

--- Assets/playerHit.cs
+++ Assets/playerHit.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerHit : MonoBehaviour
 {
     bool canDie = false;
+    public TMP_Text score;
+    int hits = 0;
+   
     // Start is called before the first frame update
     void Start()
     {
+       
+        score.text = "0";
         Debug.Log("started");
         StartCoroutine(TurkeyWait());
     }
@@ -25,7 +32,8 @@ public class playerHit : MonoBehaviour
         Debug.Log("2222222222222222222");
         if (canDie==true && other.tag=="Cookable")
         {
-
+            hits++;
+            score.text = hits.ToString();
             Debug.Log("255555555555555555555555");
         }
     }
